169f5279daa5efac7ceefb5e52d72724b3fd2bac
Caio Caminha <caio.caminha@globalpay.com>
2025-08-06 05:24:27 -0300

    updating dockerfile

---
+++ scripts/generate-docker-image-from-ubuntu-container.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+docker run -it --name my_ubuntu -v /var/run/docker.sock:/var/run/docker.sock ubuntu:22.04
+
+apt update && apt install -y docker.io
+
+apt install -y git
+
+# SDKMAN! requirements
+apt install curl
+apt install unzip
+apt install zip
+
+#Install sdkman!
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+sdk install java 21.0.2-graalce
+sdk use java 21.0.2-graalce
+
+sdk install gradle 8.7
+
+apt install build-essential zlib1g-dev -y
